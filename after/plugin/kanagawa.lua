8a757a77552ecbb3fbb048eaebb55fc32d0dd45c
AmadeusK525 <amadeusk525@protonmail.com>
2024-05-13 17:36:15 -0300

    fix(kanagawa): make bg transparent and add blocky menu theme

--- after/plugin/kanagawa.lua
+++ after/plugin/kanagawa.lua
@@ -3,6 +3,7 @@ require('kanagawa').setup({
     commentStyle = { italic = true },
     theme = "dragon",
     dimInactive = true,
+    transparent = true,
     overrides = function(colors)
         local theme = colors.theme
 
@@ -10,6 +11,11 @@ require('kanagawa').setup({
             NormalFloat = { bg = "none" },
             FloatBorder = { bg = "none" },
             FloatTitle = { bg = "none" },
+            Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1, blend = vim.o.pumblend },
+            PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+            PmenuSbar = { bg = theme.ui.bg_m1 },
+            PmenuThumb = { bg = theme.ui.bg_p2 },
+
 
             -- Save an hlgroup with dark background and dimmed foreground
             -- so that you can use it where your still want darker windows.
