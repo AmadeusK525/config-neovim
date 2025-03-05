69f05fef4f98b0b5d753e54b768a85aa149c9350
Leonardo Bronstein Franceschetti <bronstein.franceschetti@protonmail.com>
2025-03-05 14:46:01 -0300

    fix(telescope): fix version and improve settings

--- lua/amadeusk525/packer.lua
+++ lua/amadeusk525/packer.lua
@@ -8,9 +8,15 @@ return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.2',
-        -- or                            , branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' } }
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.8',
+        requires = {
+            { 'nvim-lua/plenary.nvim' },
+        },
+    }
+    use {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release',
     }
 
     use 'rebelot/kanagawa.nvim'
